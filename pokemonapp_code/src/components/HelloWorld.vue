49d30af7537676f632f1b6cace6e08a916dfda80
Souvik Das <71898027+souvikotaku@users.noreply.github.com>
2021-02-17 11:30:19 +0530

    Add files via upload
    added the files

---
+++ pokemonapp_code/src/components/HelloWorld.vue
@@ -0,0 +1,105 @@
+<template>
+<div class="App">  
+    <div>
+        <img  style="width:70%" src="../assets/pokemon.png"/>
+        <p style="color:black">Made by Souvik</p>
+    </div>
+<br/>
+
+    <!-- <button @click='fetchData'>click</button> -->
+    <div className="row shapeBox">
+        
+             <div
+              v-for="pokemon in pokemons" 
+               
+              v-bind:key="pokemon"
+              style="width:20%;padding:6px"
+              className='image-container d-flex justify-content-start'
+            >
+              <!-- <p>{{index}}. {{post.characterName}}</p> -->
+              
+             <!-- <h1>name:{{pokemon.name}}</h1> -->
+               <img 
+              class="picbox"
+              style="width:100%"
+              v-bind:src="pokemon.img" alt="No Photo"
+              />
+              <div className='overlay  '>
+                <h5 style="color:white;font-size:15px">Name: {{pokemon.name || 'nil'}}</h5>
+
+               <div
+                v-for="poketype in pokemon.type"  
+              v-bind:key="poketype"
+               >
+                 <h5 style="color:white;font-size:15px">Type: {{poketype}}</h5>
+               </div>
+               <h5 style="color:white;font-size:15px">Height: {{pokemon.height || 'nil'}}</h5>
+               <h5 style="color:white;font-size:15px">Weight: {{pokemon.weight || 'nil'}}</h5>
+                <!-- <h5 style="color:white;font-size:15px">House: {{post.houseName || 'nil'}}</h5>
+                
+                <h5 style="color:white;font-size:15px">Actor/Actress: {{post.actorName || 'nil'}}</h5> -->
+               <!-- <button 
+               className='btn btn-info'
+
+               
+               >More Info</button> -->
+               
+                </div>
+            </div> 
+        
+    </div>
+ </div> 
+</template>
+
+<script>
+
+// import {Card} from '@material-ui/core';
+
+export default {
+
+
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+      return {
+        query: '',
+        pokemons: null,
+        
+      }
+  },
+
+
+  mounted: async function(){
+      const url = `https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json`;
+      const response2 = await fetch (url);
+      const responseJson2 = await response2.json();
+      console.log(responseJson2.pokemon);
+
+      this.pokemons = responseJson2.pokemon
+
+
+
+      
+  },
+
+  
+
+  
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+@import '../assets/bootstrap.css';
+@import './pokemon.css'
+
+
+
+
+</style>
